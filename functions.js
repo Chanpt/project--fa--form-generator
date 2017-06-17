a72ed06b98169bc7964baae7e361dc321fe50fad
Luiz Felipe <frazaolipe@gmail.com>
2017-06-17 17:24:30 -0300

    [2] Modificação da forma como o JavaScript é carregado.

--- functions.js
+++ functions.js
@@ -7,4 +7,25 @@
  *  @author Luiz~
  *
  */
- 
+(function ($) {
+  'use strict';
+
+  $(function () {
+    $.get('/fdf/js/form-functions.js')
+      .done(function () {
+        console.log('[Gerador] JavaScript Principal Carregado com Sucesso!');
+      })
+      .fail(function () {
+        alert([
+          'Houve um erro ao tentar carregar o arquivo JavaScript.',
+          '',
+          'Tente verificar a URL abaixo:',
+          'https://lffg.github.io/fdf/js/form-functions.js',
+          '',
+          '[AJAX ERROR]'
+        ].join('\n'))
+        ;
+      })
+    ;
+  });
+}(jQuery));
